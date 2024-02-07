355a6ac9c315be78cc0070f2aca180bb31bd4325
trollord <mkasangottuwar@clarapath.com>
2024-02-07 10:45:58 +0530

    made changes to service file

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -12,7 +12,21 @@ export class ApiService {
   ) { }
 
   getUser(githubUsername: string) {
+    try{
     return this.httpClient.get(`https://api.github.com/users/${githubUsername}`);
+    }
+    catch (error){
+      return throwError(() => 'An error occurred while fetching the username.');
+    }
+  }
+
+  getRepo(githubUsername: string, page: number, perPage: number){
+    try{
+    return this.httpClient.get(`https://api.github.com/search/repositories?q=user:${githubUsername} in:name sort:updated-asc&page=${page}&per_page=${perPage}`)
+    }
+    catch (error){
+      return throwError(() => 'An error occurred while fetching repositories.');
+    }
   }
 
   // implement getRepos method by referring to the documentation. Add proper types for the return type and params 
