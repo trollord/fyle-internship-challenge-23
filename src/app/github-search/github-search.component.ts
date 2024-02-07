ba63d94751ffa12050b2dc28b67ca3fc89a05a14
trollord <mkasangottuwar@clarapath.com>
2024-02-07 10:45:39 +0530

    added githubSearch component

---
+++ src/app/github-search/github-search.component.ts
@@ -0,0 +1,25 @@
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-github-search',
+  templateUrl: './github-search.component.html',
+  styleUrls: ['./github-search.component.scss']
+})
+export class GithubSearchComponent implements OnInit {
+
+// To flow the data from child to parent i.e main App-component
+@Output() onSearchUser: EventEmitter<any> = new EventEmitter();
+public inputGithubUser: string = '';
+
+constructor() { }
+
+  public searchUser(){
+    this.onSearchUser.emit(this.inputGithubUser);
+    this.inputGithubUser = '';
+    
+  }
+
+  ngOnInit(): void {
+  }
+
+}
