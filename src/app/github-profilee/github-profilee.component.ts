f1b547c58e9923099997aecf9ee461297e6f875c
trollord <mkasangottuwar@clarapath.com>
2024-02-07 10:45:13 +0530

    added github Profile component

---
+++ src/app/github-profilee/github-profilee.component.ts
@@ -0,0 +1,102 @@
+import { ImplicitReceiver } from '@angular/compiler';
+import { Component, Input, OnChanges } from '@angular/core';
+import { ApiService } from '../services/api.service';
+import * as $ from 'jquery'
+
+@Component({
+  selector: 'app-github-profile',
+  templateUrl: './github-profilee.component.html',
+  styleUrls: ['./github-profilee.component.scss']
+})
+export class GithubProfileComponent implements OnChanges {
+
+  @Input() githubUsername!: string
+
+  repoFetching: boolean = false
+profileFetching: boolean = false
+
+data : any
+repos: any
+
+page: number = 1;
+count: number = 0;
+tableSize: number = 10;
+tableSizes: any = [5, 10, 15, 20 ];
+
+//injecting the serviceFunctions
+constructor(private user:ApiService) {}
+ngOnChanges(): void {
+  this.repoFetching = true;    // for alert of 'no repo found'
+  this.page = 1;      
+  this.reset();        
+  this.dataList();
+  this.listRepo()
+
+}
+
+viewProfile(){
+  this.dataList();
+}
+listRepo(){
+  this.repoList();
+}
+
+
+dataList():void{
+  this.user.getUser(this.githubUsername).subscribe({
+    next:(response) =>{
+      this.data = response;
+      this.profileFetching = false;
+    },
+    error: (err) => {
+      this.profileFetching = true;
+    },
+  });
+}
+
+
+repoList():void{
+  this.user.getRepo(this.githubUsername, this.page, this.tableSize).subscribe({
+    next:(response) =>{
+      this.repos = response;
+    this.count = this.repos.total_count;
+    this.repoFetching = false;
+    },
+    error: (err) => {
+
+    },
+  });
+}
+
+
+// To handle pagination on changing pages
+
+onTableDataChange(event: any){
+  this.repos=null;
+  $(window).scrollTop(0);
+  this.page = event;
+  this.repoList();
+}
+
+onTableSizeChange(event: any): void{
+  this.tableSize = event.target.value;
+  this.page =1;
+  this.repoList();
+}
+
+reset(){
+  this.data=null;
+  this.repos=null;
+}
+
+
+// As Github Api returns only first 1000 repositories, 
+// this fuctions is set the limit on Search of Repositories
+
+RepoLimiter(): number {
+  if (this.repos.total_count! > 1000) {
+    return (this.repos.total_count = 1000);
+  }
+  return this.repos.total_count;
+}
+}
