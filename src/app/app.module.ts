ec27b03959badc8d5b3205f69bcf406e440b5158
trollord <mkasangottuwar@clarapath.com>
2024-02-07 10:46:32 +0530

    app component configured

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from  '@angular/common/http';
+import { GithubSearchComponent } from './github-search/github-search.component';
+import { GithubProfileComponent } from './github-profilee/github-profilee.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GithubSearchComponent,
+    GithubProfileComponent,
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
